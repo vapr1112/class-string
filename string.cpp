19c030a836440c07c6174b61ccf1da19ddeca510
vapr1112 <rlu24des.com@gmail.com>
2025-01-16 15:53:30 +0300

    вынесены перегрузки методов из класса

--- string.cpp
+++ string.cpp
@@ -29,6 +29,59 @@ strings::strings(strings&& str_p) : str{ str_p.str }, size{ str_p.size }
 	str_p.size = 0;
 }
 
+void strings:: set_size(const int size_p)
+{
+	size = size_p;
+
+	delete[] str;
+	str = new char[size];
+}
+
+void strings:: print() const
+{
+	puts(str);
+	cout << "размер строки " << size << "\n";
+}
+
+char strings:: operator[](const int idx)const
+{
+	//assert дает возможность отслеживать недопустимый индекс, взят из методички
+	assert(idx > 0 && idx < size && "\nнедопустимый индекс");
+	return str[idx];
+}
+
+char& strings:: operator[](const int idx)
+{
+	assert(idx > 0 && idx < size && "\nнедопустимый индекс");
+	return str[idx];
+}
+
+char* strings:: operator[](const char* str_p)
+{
+	//ищется подстрока в строке, сохраняется указатель
+	char* idx{ strstr(str, str_p) };
+	//создается буферная строка
+	char* buf_str = new char[strlen(str_p) + 1];
+	if (idx != 0)
+	{
+		//в буферную строку копируется указатель на подстроку
+		buf_str = idx;
+		//возвращается буферная строка
+		return buf_str;
+	}
+}
+
+const char* strings:: operator[](const char* str_p)const
+{
+	char* idx{ strstr(str, str_p) };
+	char* buf_str = new char[strlen(str_p) + 1];
+	if (idx != 0)
+	{
+		buf_str = idx;
+		return buf_str;
+	}
+}
+
 void strings:: set_str(const char* str_p)
 {
 	int size_p = strlen(str_p) + 1;
@@ -50,4 +103,29 @@ void strings:: set_str_adress(char* adress_str, const char* entered_string)
 
 	strcpy_s(adress_str, strlen(entered_string) + 1, entered_string);//копируется в подстроку 1 подстрока 2
 	adress_str[strlen(adress_str)] = symbol;//возвращается сохраненный символ на место \0
+}
+
+int strings:: operator()(const char symbol)
+{
+	if (strchr(str, symbol) != 0)
+	{
+		//создается буферная строка, в которую копируется значение из str
+		char* buf_str = new char[size];
+		strcpy_s(buf_str, size, str);
+
+		//создается указатель на \0
+		const char* ptr = "\0";
+
+		// на месте переданного символа в буферной ставится \0
+		*(strchr(buf_str, symbol)) = *(ptr);
+
+		//размер полученной буферной строки и есть индекс искомого символа
+		int index = strlen(buf_str);
+
+		delete[] buf_str;
+
+		return index;
+	}
+
+	return -1;
 }
--- string.h
+++ string.h
@@ -18,126 +18,35 @@ public:
 
 	// главный конструктор, сoздает строку и инициализирует ее
 	strings(const char* str_p, const int size_p); 
-
 	//конструктор, создающий строку, длинной 80
 	strings() : strings(nullptr, 80) {}; 
-
 	//конструктор, создающий строку длинной, указанной пользователем
 	strings(const int size_p) : strings(nullptr, size_p) {}; 
-
 	//конструктор копирования
 	strings(const strings& str_p);
-	
 	//конструктор переноса
 	strings(strings&& str_p);
-
 	explicit strings(const char* str_p) : strings(nullptr, strlen(str_p) + 1) {}
 
 	void set_str(const char* str_p);
-
-	void set_size(const int size_p)
-	{
-		size = size_p;
-
-		delete[] str;
-		str = new char[size];
-	}
-
-	const char* get_str() const
-	{
-		return str;
-	}
-
-	int get_size() const
-	{
-		return size;
-	}
-
-	void print() const
-	{
-		puts(str);
-		cout << "размер строки " << size << "\n";
-	}
-
+	void set_size(const int size_p);
+	const char* get_str() const {return str}
+	int get_size() const {return size;}
+	void print() const;
 	//возвращает количество обЬектов класса
-	static int show_count() 
-	{
-		return count;
-	}
-
+	static int show_count() {return count;}
 	//меняет подстроку в строке на указанную подстроку
 	void set_str_adress(char* adress_str, const char* entered_string);
-
 	//перегрузка специальных операторов
+	char operator[](const int idx)const;
+	char& operator[](const int idx);
+	char* operator[](const char* str_p);
 
-	char operator[](const int idx)const
-	{
-		//assert дает возможность отслеживать недопустимый индекс, взят из методички
-		assert(idx > 0 && idx < size && "\nнедопустимый индекс");
-		return str[idx];
-	}
+	const char* operator[](const char* str_p)const;
 
-	char& operator[](const int idx)
-	{
-		assert(idx > 0 && idx < size && "\nнедопустимый индекс");
-		return str[idx];
-	}
+	int operator()(const char symbol);
 
-	char* operator[](const char* str_p)
-	{
-		//ищется подстрока в строке, сохраняется указатель
-		char* idx{ strstr(str, str_p) };
-		//создается буферная строка
-		char* buf_str = new char[strlen(str_p) + 1];
-		if (idx != 0)
-		{
-			//в буферную строку копируется указатель на подстроку
-			buf_str = idx;
-			//возвращается буферная строка
-			return buf_str;
-		}
-	}
-
-	const char* operator[](const char* str_p)const
-	{
-		char* idx{ strstr(str, str_p) };
-		char* buf_str = new char[strlen(str_p) + 1];
-		if (idx != 0)
-		{
-			buf_str = idx;
-			return buf_str;
-		}
-	}
-
-	int operator()(const char symbol) 
-	{
-		if (strchr(str, symbol) != 0)
-		{
-			//создается буферная строка, в которую копируется значение из str
-			char* buf_str = new char[size];
-			strcpy_s(buf_str, size, str);
-
-			//создается указатель на \0
-			const char* ptr = "\0";
-
-			// на месте переданного символа в буферной ставится \0
-			*(strchr(buf_str, symbol)) = *(ptr);
-
-			//размер полученной буферной строки и есть индекс искомого символа
-			int index = strlen(buf_str);
-
-			delete[] buf_str;
-
-			return index;
-		}
-
-		return -1;
-	}
-
-	operator int() const
-	{
-		return strlen(str) + 1;
-	}
+	operator int() const {return strlen(str) + 1;}
 
 	~strings()
 	{
